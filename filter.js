4181b37193d0d3db95489d6f1fe07c604a1a1dee
rootnure <nur.diu.2791@gmail.com>
2026-02-09 20:26:18 +0600

    No Matching Operations Example added for Array.filte() and Array.find() method

--- filter.js
+++ filter.js
@@ -2,3 +2,5 @@ const numbers = [2, 5, 7, 1, 10, 8];
 const oddNumbers = numbers.filter(number => number % 2);
 const evenNumbers = numbers.filter(number => !(number % 2));
 console.log(oddNumbers, evenNumbers); // [ 5, 7, 1 ] [ 2, 10, 8 ]
+const filterNegative = numbers.filter(number => number < 0);
+console.log(filterNegative); // []
--- find.js
+++ find.js
@@ -1,3 +1,5 @@
 const numbers = [2, 5, 7, 1, 10, 8];
 const firstOddNumber = numbers.find(number => number % 2);
 console.log(firstOddNumber); // 5
+const findFirstNegative = numbers.find(n => n < 0)
+console.log(findFirstNegative); // undefined
